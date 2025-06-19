574c0679f24ed556023e4053a3f1ccf735fe8e96
Darwin Cereska <accfordarwin@gmail.com>
2025-06-19 16:30:17 -0400

    update: quickfix

--- Formula/xswitcher.rb
+++ Formula/xswitcher.rb
@@ -3,14 +3,19 @@ class Xswitcher < Formula
   desc ""
   homepage "https://github.com/darwincereska/xswitcher"
   url "https://github.com/darwincereska/xswitcher/archive/refs/tags/v#{version}.tar.gz"
-  sha256 ""
+  sha256 "1e5e9f703cc8c7400adda8f9c96a3ae592d91ef78d932c40fd425097d88eba9e"
   license "MIT"
   
   depends_on "go" => :build
   
   def install
-    system "make", "build"
-    bin.install "xswitcher"
+    if OS.mac?
+      system "make", "build-darwin"
+    elsif OS.linux?
+      system "make", "build-linux"
+    end
+    
+    bin.install Dir["bin/xswitcher-*"].first => "xswitcher"
   end
   
   test do
