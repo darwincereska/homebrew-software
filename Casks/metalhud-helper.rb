03d49a553d4a82e64531c43bbcfc60e235d08c6b
Darwin Cereska <darwin@mac.local>
2025-07-05 17:47:33 -0400

    init: added metalhud-helper

---
+++ Casks/metalhud-helper.rb
@@ -0,0 +1,12 @@
+cask "metalhud-helper" do
+  version "1.0.2"
+  sha256 ""
+
+  url "https://github.com/oliwonders/MetalHUDHelper/releases/tag/v#{version}"
+
+  name "MetalHUDHelper"
+  desc "App for toggling the Apple Metal Performance HUD globally on macOS"
+  homepage "https://github.com/oliwonders/MetalHUDHelper"
+
+  app "MetalHUDHelper.app"
+end
