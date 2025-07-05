6e0fba2bb0e71fbf4413fdbc7bb484e044aadf59
Darwin Cereska <darwin@mac.local>
2025-07-05 17:49:04 -0400

    update: patched metalhud-helper

--- Casks/metalhud-helper.rb
+++ Casks/metalhud-helper.rb
@@ -2,7 +2,7 @@ cask "metalhud-helper" do
   version "1.0.2"
   sha256 ""
 
-  url "https://github.com/oliwonders/MetalHUDHelper/releases/tag/v#{version}"
+  url "https://github.com/oliwonders/MetalHUDHelper/releases/tag/v#{version}/MetalHUDHelper.dmg"
 
   name "MetalHUDHelper"
   desc "App for toggling the Apple Metal Performance HUD globally on macOS"
