1aab78948c9b2fd1aad4f43ad6268479ba1a20a4
darwincereska <accfordarwin@gmail.com>
2025-10-20 00:58:52 -0400

    fix(sha256): fixed hash

--- Formula/pledge.rb
+++ Formula/pledge.rb
@@ -6,20 +6,20 @@ class Pledge < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "#{homepage}/releases/download/v#{version}/pledge-darwin-arm64"
-      sha256 "8d72cc6840f9e1e995356725f198d19fd159917b6443f9daf2d0bc1e8f974ef3"
+      sha256 "3ca0ca0a04daa86ba4527b9d0ff1f3914d7f6ab6ae4a587929ee668475fa2d31"
     else
       url "#{homepage}/releases/download/v#{version}/pledge-darwin-amd64"
-      sha256 "cd4f30297bbfd0c924e41aa07ff300b335df9244dbda9914639aa077f30bd261"
+      sha256 "1714e952e562d58dfc0472a185e7650b0ee97e0a6ccf9fbba2f42e9f0c3ae0ae"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "#{homepage}/releases/download/v#{version}/pledge-linux-arm64"
-      sha256 "80f02a8f8ae06a63b22ecb88ff2e63cb40ef7350beb548755e6e475396ffbca0"
+      sha256 "d4aeaa8ac31877ee82f67ea06dbbeb86a711ba4e5963010260343c19176a66c9"
     else
       url "#{homepage}/releases/download/v#{version}/pledge-linux-amd64"
-      sha256 "f3d6f7097af87f67c5e0806cc3f3718a5d9b2e42f55f973846433b44271690ed"
+      sha256 "f810fe79f495cd6033425d207d3f4e936c319fc94c20fd53dcc86a044fc937b9"
     end
   end
 
