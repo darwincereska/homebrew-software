5e5b29de2d5be8319b2fd26cc7606c01d7945697
Darwin Cereska <157426553+darwincereska@users.noreply.github.com>
2025-11-10 20:50:25 -0500

    Update notevc.rb

--- Formula/notevc.rb
+++ Formula/notevc.rb
@@ -4,10 +4,10 @@ class Notevc < Formula
   homepage "https://notevc.io"
 
   if OS.mac?
-    url ""
+    url "https://github.com/darwincereska/notevc/releases/download/v#{version}/notevc-macos-arm64"
     sha256 "676dcc421176ad302b640b5ef07f3ee186c314f97bb81f3ae12cc5be30b9939d"
   elsif OS.linux?
-    url ""
+    url "https://github.com/darwincereska/notevc/releases/download/v#{version}/notevc-linux-x64"
     sha256 "93031aec26cceacc2a13c370f3352512f029c3cc9762936abd7dd6df0d2071e8"
   end
 
