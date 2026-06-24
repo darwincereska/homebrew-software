30946084a24e0d8ae4db5bba2ed3ef243635c9d0
darwincereska <accfordarwin@gmail.com>
2026-06-24 13:05:58 -0400

    feat: added mactoy

---
+++ Casks/mactoy.rb
@@ -0,0 +1,12 @@
+cask "mactoy" do
+  version "0.3.1"
+  sha256 "c647f082cee8ab7580dabe8aaadab72da044c2ebe72cf39ea8d8b4d4dbae809a"
+
+  url "https://github.com/cashcon57/mactoy/releases/download/#{version}/Mactoy-#{version}.dmg"
+
+  name "Mactoy"
+  desc "Native macOS app for Ventoy install and raw image flashing."
+  homepage "https://github.com/cashcon57/mactoy"
+
+  app "Mactoy.app"
+end
